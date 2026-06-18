c61f66a7e431dcd036bfcd683b22d1b0df74d746
Juhi Rathor <iamjuhirathor@gmail.com>
2026-06-18 09:36:10 +0530

    unsaved changes in next.js file

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig = {
-  reactStrictMode: false,
-  reactCompiler: true,
-  output: isGithubActions ? 'export' : undefined,
-  basePath: isGithubActions ? '/stbs-frontend' : '',
-  images: {
-    unoptimized: true,
-  },
+    reactStrictMode: false,
+    reactCompiler: true,
+    output: isGithubActions ? 'export' : undefined,
+    basePath: isGithubActions ? '/stbs-frontend' : '',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
